8c80bca84758689c0f1c4159f368b2d8cdbbf42d
luisk1989 <luiskariza@gmail.com>
2026-03-10 12:20:12 -0500

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-
-st.title("Mi aplicación")
-
-HtmlFile = open("index.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read()
-
-components.html(source_code, height=800)
